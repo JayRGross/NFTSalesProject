9b883f1b955c1d38846120fd44b563e1faff6091
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2021-11-06 11:13:52 -0400

    html js changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 
-router.use('/users', userRoutes);
+router.use('/fav', favRoutes);
+// do I need to duplicate this to userRoutes as well
 
 module.exports = router;
